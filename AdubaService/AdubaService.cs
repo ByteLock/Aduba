4143d4d9fac7bbd964172107f7f71a9809641762
Dennis Yilmaz <dy.0x8@outlook.com>
2022-09-12 03:58:32 -0400

    Amended Files

---
+++ AdubaService/AdubaService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ADUBAService
+{
+    public partial class AdubaService : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        public AdubaService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            Initialization.PreemptiveChecks.verifyConfiguration();
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            Initialization.PreemptiveChecks.verifyConfiguration();
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
