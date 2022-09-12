00b2d61c425ab9cbc37fb9ec128f018947a5e560
Dennis Yilmaz <dy.0x8@outlook.com>
2022-09-12 04:08:35 -0400

    Update install.bat

--- AdubaService/install.bat
+++ AdubaService/install.bat
@@ -1,5 +1,2 @@
-﻿setlocal
-set alias=Reg query "HKLM\Software\Microsoft\NET Framework Setup\NDP"
-FOR /F "TOKENS=6 DELIMS=\." %%A IN ('%alias%') DO set .NetVer=%%A
-ECHO The most current version of Net in use is %.NetVer%
+﻿dir %windir%\Microsoft.Net\framework\v* /b|findstr "v[1-9]"
 pause
