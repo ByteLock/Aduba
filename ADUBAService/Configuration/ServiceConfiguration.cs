4cac2cc5326af386818bb787be08244b8d2d8af7
Dennis Yilmaz <dy.0x8@outlook.com>
2022-09-12 03:43:44 -0400

    Full Upload

---
+++ ADUBAService/Configuration/ServiceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADUBAService.Configuration
+{
+    class ServiceConfiguration
+    {
+        public string serverAddress { get; set; }
+        public string serverPort { get; set; }
+
+    }
+}
---
+++ ADUBAService/Initialization/PreemptiveChecks.cs
@@ -0,0 +1,23 @@
+﻿using ADUBAService.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADUBAService.Initialization
+{
+    class PreemptiveChecks
+    {
+        public static void verifyConfiguration()
+        {
+            ServiceConfiguration sc = new ServiceConfiguration();
+            string rJson = File.ReadAllText("C:\\Users\\dy0x8\\Desktop\\Aduba\\config.json");
+            DateTime dt = DateTime.Now;
+            File.WriteAllText("C:\\Users\\dy0x8\\Desktop\\Aduba\\dump.txt", dt.ToString());
+            // File.AppendAllText("C:\\Users\\dy0x8\\Desktop\\Aduba\\dump.txt", dt.ToString());
+        }
+
+    }
+}
