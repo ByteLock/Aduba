24d0a640d5fbd36bc1456b98f571baec5732785a
Dennis Yilmaz <dy.0x8@outlook.com>
2022-09-13 21:46:17 -0400

    Apply Todo for the service

--- AdubaService/AdubaService.cs
+++ AdubaService/AdubaService.cs
@@ -6,22 +6,32 @@ namespace AdubaSerivce
 {
     public partial class AdubaService : ServiceBase
     {
-        Timer timer = new Timer();
+        Timer heart = new Timer();
 
         public AdubaService()
         {
             InitializeComponent();
         }
 
+
+        /**
+         * On start, we are looking to check and start these systems (in order):
+         *  1. Read and apply configuration to the service (replace config file and apply defaults if missing)
+         *  2. Connect to mySql database
+         *  3. Start socket server and configured port and address (multi-threaded server instead of single)
+         *  4. (maybe) apply configuration for multi-server use
+         */
         protected override void OnStart(string[] args)
         {
-            timer.Elapsed += new System.Timers.ElapsedEventHandler(OnElapsedTime);
-            timer.Interval = 1000;
-            timer.Enabled = true;
+
+
+            heart.Elapsed += new ElapsedEventHandler(OnHeartbeat);
+            heart.Interval = 1000;
+            heart.Enabled = true;
             Initialization.PreemptiveChecks.verifyConfiguration();
         }
 
-        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        private void OnHeartbeat(object source, ElapsedEventArgs e)
         {
             Initialization.PreemptiveChecks.verifyConfiguration();
         }
